4942c3b4ab13e701d95e490a3fb38beb382d3f66
saptoto <robertus.saptoto2@gmail.com>
2016-06-23 04:35:41 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>FORM BUKTI PEMBAYARAN AKBIDYO</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+ 
+<div id="container">
+     <div id="header">
+     <h1>FORM BUKTI PEMBAYARAN AKBIDYO</h1>
+         <span>Dibuat oleh Robertus S</span>
+        </div>
+ 
+        <div id="menu">
+         <a href="index.php" class="active">Home</a>
+            <a href="upload1.php">Upload</a>
+            <a href="download.php">Download</a>
+        </div>
+ 
+        <div id="content">
+         <h2>Home</h2>
+            <p>Selamat Datang!</p>
+            <p>FORM BUKTI PEMBAYARAN<strong> AKADEMI KEBIDANAN YOGYAKARTA</strong>.Silakan melakukan scan bukti pembayaran dengan menebalkan pada bagian Nama dan Nim agar bisa terbaca jelas pada
+		waktu scan.Dikolom Upload Filename diisi dengan format Nim_Nama Mahasiswa.Contoh : 12000 Clara Sinta.Ukuran Maksimal pengiriman adalah 1 Mb. <a href="http:///" target="_blank"></a> </p>
+        </div>
+    </div>
+ 
+</body>
+</html>
